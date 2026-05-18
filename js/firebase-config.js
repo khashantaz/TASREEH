b6821ee1631b31c76ca9d6c01026b861ac0139ca
khashantaz <khashantaz@gmail.com>
2026-05-18 16:26:55 -0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -12,12 +12,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:            "REPLACE_WITH_YOUR_API_KEY",
+  apiKey:            "AIzaSyC2VqjbHdYQPm9B0Sa9GvgVgeRbOs63AEI",
   authDomain:        "tasreeh-prod.firebaseapp.com",
   projectId:         "tasreeh-prod",
   storageBucket:     "tasreeh-prod.appspot.com",
-  messagingSenderId: "REPLACE_WITH_SENDER_ID",
-  appId:             "REPLACE_WITH_APP_ID"
+  messagingSenderId: "98590606477",
+  appId:             "1:98590606477:web:0bb5c52d8232ff85658ec4"
 };
 
 const app  = initializeApp(firebaseConfig);
